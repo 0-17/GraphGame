fbf5d6e2083c987b429ce3fdf54f63845ba85371
nijwons <017.work@gmail.com>
2022-06-29 17:05:30 +0100

    ads

--- GraphLib/Node.cs
+++ GraphLib/Node.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GraphLib
 {
     public class Node<CONTENT> where CONTENT : IEquatable<CONTENT>
     {
-                
+        public readonly CONTENT Content;
+        public readonly List<Node<CONTENT>> Neighbours;
+
+        public Node(CONTENT content)
+        {
+            Content = content;
+            Neighbours = new List<Node<CONTENT>>();
+        }
     }
 }
